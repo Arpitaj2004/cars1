a35dd643cc67b513a7f118f8175539e2b32bea98
Arpitaj2004 <arpitajadhav7879@gmail.com>
2025-04-08 10:38:26 +0530

    Update carss.py

--- carss.py
+++ carss.py
@@ -9,7 +9,7 @@ Original file is located at
 
 import streamlit as st
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
+
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 df=pd.read_csv("car.csv")
